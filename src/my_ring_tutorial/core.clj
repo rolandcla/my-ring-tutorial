80853bd953195d0a2d243f365173a2bdf903a0b5
Roland Clairquin <roland.clairquin@aeronomie.be>
2018-02-08 13:30:08 +0100

    Dispatch dans 'handler' + test du fonctionnement asynchrone...

--- src/my_ring_tutorial/core.clj
+++ src/my_ring_tutorial/core.clj
@@ -1,10 +1,25 @@
 (ns my-ring-tutorial.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn handler [request]
+(defn hello [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Hello World !"})
 
+(defn info [request]
+  {:status  200
+   :headers {}
+   :body    (pr-str request)})
+
+(defn handler [request]
+  (case (:uri request)
+    "/"          (hello request)
+    "/info"      (info request)
+    {:status 404}))
+
+(defn handler-async [request respond raise]
+  (respond (handler request)))
+
 (defn -main [port-nr]
-  (jetty/run-jetty handler {:port (Integer. port-nr)}))
+  (jetty/run-jetty handler {:port (Integer. port-nr)})
+  #_(jetty/run-jetty handler-async {:port (Integer. port-nr) :async? true}))
