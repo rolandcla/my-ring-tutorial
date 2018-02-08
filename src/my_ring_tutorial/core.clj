1d6ea692ac36b07f11b189750c34a8c4e8ce9968
Roland Clairquin <roland.clairquin@aeronomie.be>
2018-02-08 14:04:55 +0100

    Restructuration : Server - Application - Middleware - Handlers

--- src/my_ring_tutorial/core.clj
+++ src/my_ring_tutorial/core.clj
@@ -1,9 +1,16 @@
 (ns my-ring-tutorial.core
   (:require [ring.adapter.jetty :as jetty]))
 
+;; Middleware ---------------------------------------------------------------------
+(defn wrap-content-type [handler content-type]
+  (fn [request]
+    (-> (handler request)
+        (assoc-in [:headers "Content-Type"] content-type))))
+
+;; Handlers -----------------------------------------------------------------------
 (defn hello [request]
   {:status  200
-   :headers {"Content-Type" "text/html"}
+   :headers {}
    :body    "Hello World !"})
 
 (defn info [request]
@@ -20,6 +27,11 @@
 (defn handler-async [request respond raise]
   (respond (handler request)))
 
+;; Application --------------------------------------------------------------------
+(def app (-> handler
+             (wrap-content-type "text/html")))
+
+;; Run server ---------------------------------------------------------------------
 (defn -main [port-nr]
-  (jetty/run-jetty handler {:port (Integer. port-nr)})
+  (jetty/run-jetty app {:port (Integer. port-nr)})
   #_(jetty/run-jetty handler-async {:port (Integer. port-nr) :async? true}))
