1ab7430fe533f7189d35e449c47c0204aa8320b2
Roland Clairquin <rolandcl@azrael.fontaine>
2018-02-10 11:38:10 +0100

    Petit test de la gestion des sessions...

--- src/my_ring_tutorial/core.clj
+++ src/my_ring_tutorial/core.clj
@@ -5,7 +5,8 @@
             [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.not-modified :refer [wrap-not-modified]]
             [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.cookies :refer [wrap-cookies]]))
+            [ring.middleware.cookies :refer [wrap-cookies]]
+            [ring.middleware.session :refer [wrap-session]]))
 
 ;; Middleware ---------------------------------------------------------------------
 (defn wrap-content-type [handler content-type]
@@ -39,6 +40,13 @@
   (-> (response "Setting a cookie.")
       (set-cookie "session_id" "session_id_hash!" {:max-age 20})))
 
+(defn session [request]
+  (let [sess (:session request {})
+        cnt (:count sess 0)
+        sess (assoc sess :count (inc cnt))]
+    (-> (response (format "You accessed this page %d times." cnt))
+        (assoc :session sess))))
+
 ;; -------------------------------------
 
 (defn handler [request]
@@ -50,6 +58,7 @@
          "/rtbf"      rtbf
          "/readme"    readme
          "/cookie"    cookie
+         "/session"   session
          (fn [_] {:status 404})))))
 
 (defn handler-async [request respond raise]
@@ -63,6 +72,7 @@
              (wrap-not-modified)
              (wrap-params)
              (wrap-cookies)
+             (wrap-session {:cookie-attrs {:max-age 30}})
              ))
 
 ;; Run server ---------------------------------------------------------------------
